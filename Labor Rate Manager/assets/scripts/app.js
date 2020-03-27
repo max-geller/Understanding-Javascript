8375bffa1cfabfb7fa1022454a29b9553f16b41d
Max Geller <mxgeller@gmail.com>
2020-03-27 11:20:50 -0700

    added labor rate manager

---
+++ Labor Rate Manager/assets/scripts/app.js
@@ -0,0 +1,54 @@
+// Put Constants first, Variables second, Functions third
+
+// Trades
+let trade;
+let selectedTrade;
+function addTrade();
+
+// Unions
+let union;
+let selectedUnion;
+function addUnion();
+
+// Locals
+let local;
+let selectedLocal;
+function addLocal();
+
+// Agreements
+let agreement;
+let selectedAgreement
+let currentAgreement;
+
+function addAgreement() {
+    const newAgrement;
+};
+
+function getAgreement();
+
+function getCurretAgreement();
+
+// Classifications
+let classification;
+function addClassification();
+
+// Conditions
+let straightTime;
+let overTime;
+let doubleTime;
+
+// Rates
+let baseRate;
+
+
+
+
+
+
+
+
+
+
+
+
+
